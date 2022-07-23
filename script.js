bdedb186a07f8ae165b0e1b5460bd10fb95c80bb
yFlok <101664854+yFlok@users.noreply.github.com>
2022-07-23 19:13:41 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-rick = document.querySelector('video#cu')
-cu = document.querySelector('div#foda')
-
-function gg() {
-    rick.style.display = "block";
-    cu.style.display = "none";
-    rick.play()
-}
